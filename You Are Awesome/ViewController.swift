856e2ab1f5fbf7e2ae67e7d3ef3514532b996169
Joshua Chang <joshchangg01@gmail.com>
2021-09-06 12:19:23 -0400

    Section 2.10 Added Arrays

--- You Are Awesome/ViewController.swift
+++ You Are Awesome/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var imageView: UIImageView!
     
     var imageNumber = 0
+    var messageNumber = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,6 +20,17 @@ class ViewController: UIViewController {
    
     
     @IBAction func messageButtonPressed(_ sender: UIButton) {
+        let messages = ["You Are Awesome!",
+                        "You Are Great!",
+                        "You Are Fantastic!",
+                        "Fabulous? That's You!"]
+       
+        messageLabel.text = messages[messageNumber]
+        messageNumber += 1
+        if messageNumber == messages.count {
+            messageNumber = 0
+        }
+        
         print(imageNumber)
         let imageName = "image" + String(imageNumber)
         imageView.image = UIImage(named: imageName)
