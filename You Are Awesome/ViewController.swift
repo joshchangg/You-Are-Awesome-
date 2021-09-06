1a7a6fd172dc748e20887d9735343af64375cb34
Joshua Chang <joshchangg01@gmail.com>
2021-09-06 11:34:47 -0400

    Section 2.9 Loaded All Images and added Type Conversion

--- You Are Awesome/ViewController.swift
+++ You Are Awesome/ViewController.swift
@@ -11,28 +11,38 @@ class ViewController: UIViewController {
     @IBOutlet weak var messageLabel: UILabel!
     @IBOutlet weak var imageView: UIImageView!
     
+    var imageNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
    
     
     @IBAction func messageButtonPressed(_ sender: UIButton) {
-        let awesomeMessage = "You Are Awesome!"
-        let greatMessage = "You Are Great!"
-        let bombMessage = "You are Da Bomb!"
+        print(imageNumber)
+        let imageName = "image" + String(imageNumber)
+        imageView.image = UIImage(named: imageName)
+        imageNumber = imageNumber + 1
+        if imageNumber == 10 {
+            imageNumber = 0
+        }
         
-        if messageLabel.text == awesomeMessage {
-            messageLabel.text = greatMessage
-            imageView.image = UIImage(named: "image1")
-        } else if messageLabel.text == greatMessage {
-            messageLabel.text = bombMessage
-            imageView.image = UIImage(named: "image2")
-        } else {
-            messageLabel.text = awesomeMessage
-            imageView.image = UIImage(named: "image0")
-            
+//        let awesomeMessage = "You Are Awesome!"
+//        let greatMessage = "You Are Great!"
+//        let bombMessage = "You are Da Bomb!"
+//
+//        if messageLabel.text == awesomeMessage {
+//            messageLabel.text = greatMessage
+//            imageView.image = UIImage(named: "image1")
+//        } else if messageLabel.text == greatMessage {
+//            messageLabel.text = bombMessage
+//            imageView.image = UIImage(named: "image2")
+//        } else {
+//            messageLabel.text = awesomeMessage
+//            imageView.image = UIImage(named: "image0")
+
             
         }
     }
-}
+
 
