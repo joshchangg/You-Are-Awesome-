3c2d52997d6e636032a05f603d99b225b47e0792
Joshua Chang <joshchangg01@gmail.com>
2021-09-06 11:25:06 -0400

    Section 2.8 Added conditional statements and let for variables

--- You Are Awesome/ViewController.swift
+++ You Are Awesome/ViewController.swift
@@ -13,16 +13,26 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        messageLabel.text = ""
-        // Do any additional setup after loading the view.
-        print("viewDidLoad has run!")
-        messageLabel.text = "Fabulous? That's You!"
     }
    
     
     @IBAction func messageButtonPressed(_ sender: UIButton) {
-        messageLabel.text = "You Are Awesome!"
-        imageView.image = UIImage(named: "image0")
+        let awesomeMessage = "You Are Awesome!"
+        let greatMessage = "You Are Great!"
+        let bombMessage = "You are Da Bomb!"
+        
+        if messageLabel.text == awesomeMessage {
+            messageLabel.text = greatMessage
+            imageView.image = UIImage(named: "image1")
+        } else if messageLabel.text == greatMessage {
+            messageLabel.text = bombMessage
+            imageView.image = UIImage(named: "image2")
+        } else {
+            messageLabel.text = awesomeMessage
+            imageView.image = UIImage(named: "image0")
+            
+            
+        }
     }
 }
 
