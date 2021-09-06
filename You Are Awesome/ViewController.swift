4480892acb072195736f7fe1d6f4cac22a776fed
Joshua Chang <joshchangg01@gmail.com>
2021-09-06 12:45:46 -0400

    Section 2.11 Added Randomization and Array Shuffling

--- You Are Awesome/ViewController.swift
+++ You Are Awesome/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     
     var imageNumber = 0
     var messageNumber = 0
-    
+    let totalNumberOfImages = 9
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -23,21 +23,26 @@ class ViewController: UIViewController {
         let messages = ["You Are Awesome!",
                         "You Are Great!",
                         "You Are Fantastic!",
-                        "Fabulous? That's You!"]
-       
-        messageLabel.text = messages[messageNumber]
-        messageNumber += 1
-        if messageNumber == messages.count {
-            messageNumber = 0
-        }
+                        "When the Genius bar needs help, they call you!",
+                        "Fabulous? That's You!",
+                        "You've Got the Design Skills of Jony Ive"]
         
-        print(imageNumber)
-        let imageName = "image" + String(imageNumber)
-        imageView.image = UIImage(named: imageName)
-        imageNumber = imageNumber + 1
-        if imageNumber == 10 {
-            imageNumber = 0
-        }
+        messageLabel.text = (messages[Int.random(in:0...messages.count-1)])
+        imageView.image = UIImage(named: "image\(Int.random(in: 0...totalNumberOfImages))")
+
+//        messageLabel.text = messages[messageNumber]
+//        messageNumber += 1
+//        if messageNumber == messages.count {
+//            messageNumber = 0
+//        }
+//
+//        print(imageNumber)
+//        let imageName = "image" + String(imageNumber)
+//        imageView.image = UIImage(named: imageName)
+//        imageNumber = imageNumber + 1
+//        if imageNumber == 10 {
+//            imageNumber = 0
+//        }
         
 //        let awesomeMessage = "You Are Awesome!"
 //        let greatMessage = "You Are Great!"
